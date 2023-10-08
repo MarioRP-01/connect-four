23bc3f24350eadf762d32e3dfbb9f836fb8906d9
MarioRP-01 <marlorosadron@gmail.com>
2023-10-08 09:38:23 +0200

    Create new type to range numerics. Create class Coordinates

--- src/models/Coordinates.ts
+++ src/models/Coordinates.ts
@@ -1,3 +1,4 @@
+import { type IntRange } from '../utils/IntRange.ts'
 import { type ObjectValues } from '../utils/objectValues.ts'
 
 export const VALID_COORDINATES = {
@@ -7,7 +8,23 @@ export const VALID_COORDINATES = {
 
 export type validCoordinates = ObjectValues<typeof VALID_COORDINATES>
 
-export interface Coordinates {
-  row: number
-  column: number
+export class Coordinates {
+  readonly row: number
+  readonly column: number
+
+  constructor (
+    row: IntRange<0, typeof VALID_COORDINATES.MAX_ROW>,
+    column: IntRange<0, typeof VALID_COORDINATES.MAX_COLUMN>
+  ) {
+    if (row < 1 || row > VALID_COORDINATES.MAX_ROW) {
+      throw new Error(`row ${row} is not valid`)
+    }
+
+    if (column < 1 || column > VALID_COORDINATES.MAX_COLUMN) {
+      throw new Error(`column ${column} is not valid`)
+    }
+
+    this.row = row
+    this.column = column
+  }
 }
