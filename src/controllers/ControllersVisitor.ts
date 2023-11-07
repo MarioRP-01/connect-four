5dc72c0ef282f5db8acbb390e8c4121069b1a8f1
MarioRP-01 <marlorosadron@gmail.com>
2023-11-07 17:13:50 +0100

    Remove unused ControllersVisitor

--- src/controllers/ControllersVisitor.ts
+++
@@ -1,9 +0,0 @@
-import { type PlayController } from './PlayController'
-import { type ResultController } from './ResultController'
-import { type StartController } from './StartController'
-
-export interface ControllersVisitor {
-  visitStartController: (controller: StartController) => void
-  visitPlayController: (controller: PlayController) => Promise<void>
-  visitResultController: (controller: ResultController) => void
-}
