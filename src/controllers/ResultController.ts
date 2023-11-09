7fb35df49d70cb041a79c8ee20fe65addbbb5ada
MarioRP-01 <marlorosadron@gmail.com>
2023-11-09 17:25:58 +0100

    Remove unnecesary function getWinner in ResultController

--- src/controllers/ResultController.ts
+++ src/controllers/ResultController.ts
@@ -1,14 +1,9 @@
-import { type Player } from '../models/Player.ts'
 import { Controller } from './Controller.ts'
 import { type LogicController } from './LogicController.ts'
 
 export class ResultController extends Controller implements LogicController {
-  private getWinner (): Player | null {
-    return this.session.getWinner()
-  }
-
   async control (): Promise<void> {
-    this.viewFactory.createResultView().interact(this.getWinner())
+    this.viewFactory.createResultView().interact(this.session.getWinner())
     this.nextState()
   }
 }
