5716f4a526d17e03e75404bb6bd94acf19e10149
Marcela Ramirez <marcela.ramirez.ext@adevinta.com>
2023-11-19 19:06:04 +0100

    WIP

--- test/models/Line.test.ts
+++ test/models/Line.test.ts
@@ -1,15 +1,87 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { suite, test } from '@testdeck/jest'
 import { Coordinate } from '../../src/utils/Coordinate'
 import { type DirectionType, type Line } from '../../src/utils/Line'
 import { LineFactory } from '../../src/models/Line'
 
-describe('LineFactory', () => {
-  const sut: LineFactory = new LineFactory()
+const sut: LineFactory = new LineFactory()
 
-  describe('createLineFromCoordinate', () => {
-    it('creates a horizontal line with the correct length and coordinate values', () => {
-      const initialCoordinate = new Coordinate(0, 0)
-      const directionType: DirectionType = 'HORIZONTAL'
-      const expectedLine: Line = [
+@suite
+class LineFactoryTest {
+  @test
+  createsHorizontalLineWithCorrectLengthAndValues (): void {
+    const initialCoordinate = new Coordinate(0, 0)
+    const directionType: DirectionType = 'HORIZONTAL'
+    const expectedLine: Line = [
+      new Coordinate(0, 0),
+      new Coordinate(0, 1),
+      new Coordinate(0, 2),
+      new Coordinate(0, 3),
+      new Coordinate(0, 4),
+      new Coordinate(0, 5),
+      new Coordinate(0, 6)
+    ]
+
+    const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
+
+    expect(line).toEqual(expectedLine)
+  }
+
+  @test
+  createsVerticalLineWithCorrectLengthAndValues (): void {
+    const initialCoordinate = new Coordinate(0, 0)
+    const directionType: DirectionType = 'VERTICAL'
+    const expectedLine: Line = [
+      new Coordinate(0, 0),
+      new Coordinate(1, 0),
+      new Coordinate(2, 0),
+      new Coordinate(3, 0),
+      new Coordinate(4, 0),
+      new Coordinate(5, 0)
+    ]
+
+    const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
+
+    expect(line).toEqual(expectedLine)
+  }
+
+  @test
+  createsAscendingDiagonalLineWithCorrectLengthAndValues (): void {
+    const initialCoordinate = new Coordinate(0, 0)
+    const directionType: DirectionType = 'ASCENDING_DIAGONAL'
+    const expectedLine: Line = [
+      new Coordinate(0, 0),
+      new Coordinate(1, 1),
+      new Coordinate(2, 2),
+      new Coordinate(3, 3),
+      new Coordinate(4, 4),
+      new Coordinate(5, 5)
+    ]
+
+    const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
+
+    expect(line).toEqual(expectedLine)
+  }
+
+  @test
+  createsDescendingDiagonalLineWithCorrectLengthAndValues (): void {
+    const initialCoordinate = new Coordinate(0, 0)
+    const directionType: DirectionType = 'DESCENDING_DIAGONAL'
+    const expectedLine: Line = [
+      new Coordinate(0, 0)
+    ]
+
+    const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
+
+    expect(line).toEqual(expectedLine)
+  }
+
+  @test
+  createsAllLinesFromCoordinate (): void {
+    const coordinate = new Coordinate(0, 0)
+    const expectedLines: Line[] = [
+      [
         new Coordinate(0, 0),
         new Coordinate(0, 1),
         new Coordinate(0, 2),
@@ -17,103 +89,30 @@ describe('LineFactory', () => {
         new Coordinate(0, 4),
         new Coordinate(0, 5),
         new Coordinate(0, 6)
-      ]
-
-      const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
-
-      expect(line).toEqual(expectedLine)
-    })
-  })
-
-  describe('createLineFromCoordinate', () => {
-    it('creates a vertical line with the correct length and coordinate values', () => {
-      const initialCoordinate = new Coordinate(0, 0)
-      const directionType: DirectionType = 'VERTICAL'
-      const expectedLine: Line = [
+      ],
+      [
         new Coordinate(0, 0),
         new Coordinate(1, 0),
         new Coordinate(2, 0),
         new Coordinate(3, 0),
         new Coordinate(4, 0),
         new Coordinate(5, 0)
-      ]
-
-      const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
-
-      expect(line).toEqual(expectedLine)
-    })
-  })
-
-  describe('createLineFromCoordinate', () => {
-    it('creates an ascending diagonal line with the correct length and coordinate values', () => {
-      const initialCoordinate = new Coordinate(0, 0)
-      const directionType: DirectionType = 'ASCENDING_DIAGONAL'
-      const expectedLine: Line = [
+      ],
+      [
         new Coordinate(0, 0),
         new Coordinate(1, 1),
         new Coordinate(2, 2),
         new Coordinate(3, 3),
         new Coordinate(4, 4),
         new Coordinate(5, 5)
-      ]
-
-      const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
-
-      expect(line).toEqual(expectedLine)
-    })
-  })
-
-  describe('createLineFromCoordinate', () => {
-    it('creates a descending diagonal line with the correct length and coordinate values', () => {
-      const initialCoordinate = new Coordinate(0, 0)
-      const directionType: DirectionType = 'DESCENDING_DIAGONAL'
-      const expectedLine: Line = [
+      ],
+      [
         new Coordinate(0, 0)
       ]
+    ]
 
-      const line = sut.createLineFromCoordinate(initialCoordinate, directionType)
+    const lines = sut.createAllLinesFromCoordinate(coordinate)
 
-      expect(line).toEqual(expectedLine)
-    })
-  })
-
-  describe('createAllLinesFromCoordinate', () => {
-    it('creates all possible lines from a coordinate', () => {
-      const coordinate = new Coordinate(0, 0)
-      const expectedLines: Line[] = [
-        [
-          new Coordinate(0, 0),
-          new Coordinate(0, 1),
-          new Coordinate(0, 2),
-          new Coordinate(0, 3),
-          new Coordinate(0, 4),
-          new Coordinate(0, 5),
-          new Coordinate(0, 6)
-        ],
-        [
-          new Coordinate(0, 0),
-          new Coordinate(1, 0),
-          new Coordinate(2, 0),
-          new Coordinate(3, 0),
-          new Coordinate(4, 0),
-          new Coordinate(5, 0)
-        ],
-        [
-          new Coordinate(0, 0),
-          new Coordinate(1, 1),
-          new Coordinate(2, 2),
-          new Coordinate(3, 3),
-          new Coordinate(4, 4),
-          new Coordinate(5, 5)
-        ],
-        [
-          new Coordinate(0, 0)
-        ]
-      ]
-
-      const lines = sut.createAllLinesFromCoordinate(coordinate)
-
-      expect(lines).toEqual(expectedLines)
-    })
-  })
-})
+    expect(lines).toEqual(expectedLines)
+  }
+}
--- test/utils/Coordinate.test.ts
+++ test/utils/Coordinate.test.ts
@@ -4,74 +4,79 @@ import { suite, test } from '@testdeck/jest'
 import { MAX_COORDINATES, Coordinate as Sut } from '../../src/utils/Coordinate'
 import { ascendingDiagonalDirection, descendingDiagonalDirection, horizontalDirection, verticalDirection } from '../../src/utils/Line'
 
-@suite
-class CoordinateTests {
-  maxColumn = MAX_COORDINATES.COLUMN - 1
-  maxRow = MAX_COORDINATES.ROW - 1
-  minColumn = 0
-  minRow = 0
-  offset = 1
+describe('Coordinate', () => {
+  const maxColumn = MAX_COORDINATES.COLUMN - 1
+  const maxRow = MAX_COORDINATES.ROW - 1
+  const minColumn = 0
+  const minRow = 0
+  const offset = 1
 
-  @test
-  throwsErrorWhenRowAndColumnAreInvalid (): void {
-    expect(() => new Sut(this.minRow - this.offset, this.minRow - this.offset)).toThrow('row and column are not valid')
-    expect(() => new Sut(this.maxRow + this.offset, this.maxColumn + this.offset)).toThrow('row and column are not valid')
+  @suite
+  class Creation {
+    @test
+    throwsErrorWhenRowAndColumnAreInvalid (): void {
+      expect(() => new Sut(minRow - offset, minRow - offset)).toThrow('row and column are not valid')
+      expect(() => new Sut(maxRow + offset, maxColumn + offset)).toThrow('row and column are not valid')
+    }
+
+    @test
+    throwsErrorWhenOnlyRowIsInvalid (): void {
+      expect(() => new Sut(maxRow + offset, 0)).toThrow('row is not valid')
+      expect(() => new Sut(minRow - offset, 0)).toThrow('row is not valid')
+    }
+
+    @test
+    throwsErrorWhenOnlyColumnIsInvalid (): void {
+      expect(() => new Sut(0, maxColumn + offset)).toThrow('column is not valid')
+      expect(() => new Sut(0, minColumn - offset)).toThrow('column is not valid')
+    }
+
+    @test
+    instantiatesCoordinatWhenRowAndColumnAreValid (): void {
+      expect(() => new Sut(0, 0)).not.toThrow()
+    }
   }
 
-  @test
-  throwsErrorWhenOnlyRowIsInvalid (): void {
-    expect(() => new Sut(this.maxRow + this.offset, 0)).toThrow('row is not valid')
-    expect(() => new Sut(this.minRow - this.offset, 0)).toThrow('row is not valid')
+  @suite
+  class Navigation {
+    @test
+    returnsNextCoordinateOfGivenDirectionWhenItExist (): void {
+      const sut = new Sut(1, 1)
+
+      expect(sut.getNext(horizontalDirection())).toEqual(new Sut(1, 2))
+      expect(sut.getNext(ascendingDiagonalDirection())).toEqual(new Sut(2, 2))
+      expect(sut.getNext(descendingDiagonalDirection())).toEqual(new Sut(2, 0))
+      expect(sut.getNext(verticalDirection())).toEqual(new Sut(2, 1))
+    }
+
+    @test
+    returnsUndefinedWhenNextCoordinateOfGivenDirectionDoesNotExist (): void {
+      const topRightSut = new Sut(maxRow, maxColumn)
+
+      expect(topRightSut.getNext(horizontalDirection())).toBeUndefined()
+      expect(topRightSut.getNext(ascendingDiagonalDirection())).toBeUndefined()
+      expect(topRightSut.getNext(descendingDiagonalDirection())).toBeUndefined()
+      expect(topRightSut.getNext(verticalDirection())).toBeUndefined()
+    }
+
+    @test
+    returnsPreviousCoordinateOfGivenDirectionDoesNotExist (): void {
+      const sut = new Sut(1, 1)
+
+      expect(sut.getPrevious(horizontalDirection())).toEqual(new Sut(1, 0))
+      expect(sut.getPrevious(ascendingDiagonalDirection())).toEqual(new Sut(0, 0))
+      expect(sut.getPrevious(descendingDiagonalDirection())).toEqual(new Sut(0, 2))
+      expect(sut.getPrevious(verticalDirection())).toEqual(new Sut(0, 1))
+    }
+
+    @test
+    returnsUndefinedWhenPreviousCoordinateOfGivenDirectionDoesNotExist (): void {
+      const bottomLeftSut = new Sut(minRow, minColumn)
+
+      expect(bottomLeftSut.getPrevious(horizontalDirection())).toBeUndefined()
+      expect(bottomLeftSut.getPrevious(ascendingDiagonalDirection())).toBeUndefined()
+      expect(bottomLeftSut.getPrevious(descendingDiagonalDirection())).toBeUndefined()
+      expect(bottomLeftSut.getPrevious(verticalDirection())).toBeUndefined()
+    }
   }
-
-  @test
-  throwsErrorWhenOnlyColumnIsInvalid (): void {
-    expect(() => new Sut(0, this.maxColumn + this.offset)).toThrow('column is not valid')
-    expect(() => new Sut(0, this.minColumn - this.offset)).toThrow('column is not valid')
-  }
-
-  @test
-  instantiatesCoordinatWhenRowAndColumnAreValid (): void {
-    expect(() => new Sut(0, 0)).not.toThrow()
-  }
-
-  @test
-  returnsNextCoordinateOfGivenDirectionWhenItExist (): void {
-    const sut = new Sut(1, 1)
-
-    expect(sut.getNext(horizontalDirection())).toEqual(new Sut(1, 2))
-    expect(sut.getNext(ascendingDiagonalDirection())).toEqual(new Sut(2, 2))
-    expect(sut.getNext(descendingDiagonalDirection())).toEqual(new Sut(2, 0))
-    expect(sut.getNext(verticalDirection())).toEqual(new Sut(2, 1))
-  }
-
-  @test
-  returnsUndefinedWhenNextCoordinateOfGivenDirectionDoesNotExist (): void {
-    const topRightSut = new Sut(this.maxRow, this.maxColumn)
-
-    expect(topRightSut.getNext(horizontalDirection())).toBeUndefined()
-    expect(topRightSut.getNext(ascendingDiagonalDirection())).toBeUndefined()
-    expect(topRightSut.getNext(descendingDiagonalDirection())).toBeUndefined()
-    expect(topRightSut.getNext(verticalDirection())).toBeUndefined()
-  }
-
-  @test
-  returnsPreviousCoordinateOfGivenDirectionDoesNotExist (): void {
-    const sut = new Sut(1, 1)
-
-    expect(sut.getPrevious(horizontalDirection())).toEqual(new Sut(1, 0))
-    expect(sut.getPrevious(ascendingDiagonalDirection())).toEqual(new Sut(0, 0))
-    expect(sut.getPrevious(descendingDiagonalDirection())).toEqual(new Sut(0, 2))
-    expect(sut.getPrevious(verticalDirection())).toEqual(new Sut(0, 1))
-  }
-
-  @test
-  returnsUndefinedWhenPreviousCoordinateOfGivenDirectionDoesNotExist (): void {
-    const bottomLeftSut = new Sut(this.minRow, this.minColumn)
-
-    expect(bottomLeftSut.getPrevious(horizontalDirection())).toBeUndefined()
-    expect(bottomLeftSut.getPrevious(ascendingDiagonalDirection())).toBeUndefined()
-    expect(bottomLeftSut.getPrevious(descendingDiagonalDirection())).toBeUndefined()
-    expect(bottomLeftSut.getPrevious(verticalDirection())).toBeUndefined()
-  }
-}
+})
