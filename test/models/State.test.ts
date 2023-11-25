6729551f57789579409c151e9b420ac5133734c6
Marcela Ramirez <marcela.ramirez.ext@adevinta.com>
2023-11-25 10:25:05 +0100

    Add test for state model

---
+++ test/models/State.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { suite, test } from '@testdeck/jest'
+import { STATE_VALUE, State } from '../../src/models/State'
+
+describe('State', () => {
+  @suite
+  class StateTest {
+    private sut!: State
+
+    before (): void {
+      this.sut = new State()
+    }
+
+    @test
+    next_changes_state_to_next_state (): void {
+      this.sut.next()
+      expect(this.sut.getStateValue()).toBe(STATE_VALUE.PLAYING)
+      this.sut.next()
+      expect(this.sut.getStateValue()).toBe(STATE_VALUE.RESULT)
+    }
+
+    @test
+    next_does_not_changes_state_when_state_is_exit (): void {
+      this.sut.next()
+      this.sut.next()
+      this.sut.next()
+      expect(this.sut.getStateValue()).toBe(STATE_VALUE.EXIT)
+      this.sut.next()
+      expect(this.sut.getStateValue()).toBe(STATE_VALUE.EXIT)
+    }
+  }
+})
